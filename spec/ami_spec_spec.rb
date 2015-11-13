61094a08b55077005a78498a9e6c1b4c777a2f03
Patrick Robinson <patrick.robinson@envato.com>
2015-11-13 11:05:11 +1100

    Add specs for wait_for_ssh
    Fix bug in failing to raise an exception when retries are exceeded

--- spec/ami_spec_spec.rb
+++ spec/ami_spec_spec.rb
@@ -16,31 +16,58 @@ describe AmiSpec do
     )
   end
 
-  before do
-    allow(AmiSpec::AwsInstance).to receive(:start).and_return(ec2_double)
-    allow(AmiSpec::ServerSpec).to receive(:run).and_return(test_result)
-    allow(ec2_double).to receive(:terminate).and_return(true)
-    allow(ec2_double).to receive(:private_ip_address).and_return('127.0.0.1')
-    allow_any_instance_of(Object).to receive(:sleep)
+  describe '#run' do
+    before do
+      allow(AmiSpec::AwsInstance).to receive(:start).and_return(ec2_double)
+      allow(AmiSpec::ServerSpec).to receive(:run).and_return(test_result)
+      allow(ec2_double).to receive(:terminate).and_return(true)
+      allow(ec2_double).to receive(:private_ip_address).and_return('127.0.0.1')
+      allow_any_instance_of(Object).to receive(:sleep)
+    end
+
+    context 'successful tests' do
+      it 'calls aws instance for each ami' do
+        expect(AmiSpec::AwsInstance).to receive(:start).with(hash_including(role: 'web_server'))
+        expect(AmiSpec::AwsInstance).to receive(:start).with(hash_including(role: 'db_server'))
+        subject
+      end
+
+      it 'returns true' do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context 'failed tests' do
+      let(:test_result) { false }
+
+      it 'returns false' do
+        expect(subject).to be_falsey
+      end
+    end
   end
 
-  context 'successful tests' do
-    it 'calls aws instance for each ami' do
-      expect(AmiSpec::AwsInstance).to receive(:start).with(hash_including(role: 'web_server'))
-      expect(AmiSpec::AwsInstance).to receive(:start).with(hash_including(role: 'db_server'))
+  describe '#wait_for_ssh' do
+    subject { described_class.wait_for_ssh(:ip => '127.0.0.1', :user => 'ubuntu', :key_file =>'key.pem') }
+
+    it 'returns after one attempt if ssh connection succeeds' do
+      expect(Net::SSH).to receive(:start)
+
       subject
     end
 
-    it 'returns true' do
-      expect(subject).to be_truthy
-    end
-  end
+    context 'ssh fails' do
+      before do
+        allow(Net::SSH).to receive(:start).and_raise(Errno::ECONNREFUSED, 'ssh failed')
+      end
 
-  context 'failed tests' do
-    let(:test_result) { false }
+      it 'raises an exception' do
+        expect{subject}.to raise_error(AmiSpec::InstanceConnectionTimeout)
+      end
 
-    it 'returns false' do
-      expect(subject).to be_falsey
+      it 'returns the last error' do
+        expect(Net::SSH).to receive(:start).and_raise(Errno::ECONNREFUSED, 'some other error')
+        expect{subject}.to raise_error(AmiSpec::InstanceConnectionTimeout, /ssh failed/)
+      end
     end
   end
 end

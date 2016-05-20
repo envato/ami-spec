91112b8fbfaddae7207c3d47048bd41f8dc0eb57
Patrick Robinson <patrick.robinson@envato.com>
2016-05-20 10:59:29 +1000

    Fail if SSH fails, don't retry

--- lib/ami_spec/wait_for_rc.rb
+++ lib/ami_spec/wait_for_rc.rb
@@ -3,21 +3,12 @@ require 'net/ssh'
 module AmiSpec
   class WaitForRC
     def self.wait(ip_address, user, key)
-      loop do
-        begin
-          Net::SSH.start(ip_address, user, keys: [key], paranoid: false) do |ssh|
-            # Wait for SystemV to start
-            # This only works for Ubuntu with upstart.
-            # Detecting OS and Release will need something like this
-            # https://github.com/mizzy/specinfra/blob/master/lib/specinfra/helper/detect_os/debian.rb
-            ssh.exec 'while /usr/sbin/service rc status | grep -q "^rc start/running, process"; do sleep 1; done'
-          end
-          break
-        rescue Errno::ETIMEDOUT, Errno::ECONNREFUSED, Timeout::Error => error
-          sleep 1
-        else
-          break
-        end
+      Net::SSH.start(ip_address, user, keys: [key], paranoid: false) do |ssh|
+        # Wait for SystemV to start
+        # This only works for Ubuntu with upstart.
+        # Detecting OS and Release will need something like this
+        # https://github.com/mizzy/specinfra/blob/master/lib/specinfra/helper/detect_os/debian.rb
+        ssh.exec 'while /usr/sbin/service rc status | grep -q "^rc start/running, process"; do sleep 1; done'
       end
     end
   end
